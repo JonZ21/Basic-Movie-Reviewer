3d24bde1bb4c4da1a4255315045d1d61a2aaba6e
Jonathan Zhou <Jonathanzhou8@gmail.com>
2022-12-07 20:22:38 -0500

    Finished Tutorial - working
    needs syling help

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,42 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 function App() {
+  //states?
   const [movieName, setMovieName] = useState("");
   const [review, setReview] = useState("");
+  const [reviewList, setReviewList] = useState([]);
+  const [newReview, setNewReview] = useState([]);
+
+  //request
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setReviewList(response.data);
+    });
+  }, []);
 
   const submitReview = () => {
     Axios.post("http://localhost:3001/api/insert", {
       movieName: movieName,
       movieReview: review,
-    }).then(() => {
-      alert("successful insert");
     });
+
+    setReviewList([
+      ...reviewList,
+      { movieName: movieName, movieReview: review },
+    ]);
+  };
+
+  //function
+  const deleteReview = (movie) => {
+    Axios.delete(`http://localhost:3001/api/delete/${movie}`);
+  };
+
+  const updateReview = (movie) => {
+    Axios.put("http://localhost:3001/api/update", {
+      movieName: movie,
+      movieReview: newReview,
+    });
+    setNewReview("");
   };
 
   return (
@@ -37,6 +63,36 @@ function App() {
         />
 
         <button onClick={submitReview}>Submit Review</button>
+        {reviewList.map((value) => {
+          return (
+            <div className="card">
+              <h1>{value.movieName} </h1>
+              <p>{value.movieReview}</p>
+
+              <button
+                onClick={() => {
+                  deleteReview(value.movieName);
+                }}
+              >
+                Delete
+              </button>
+              <input
+                type="text"
+                id="updateInput"
+                onChange={(e) => {
+                  setNewReview(e.target.value);
+                }}
+              />
+              <button
+                onClick={() => {
+                  updateReview(value.movieName);
+                }}
+              >
+                Update
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -20,6 +20,13 @@ app.use(bodyParser.urlencoded({ extended: true })); // middleware?
 app.use(cors());
 app.use(express.json());
 
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM movie_reviews";
+  db.query(sqlSelect, (error, result) => {
+    res.send(result);
+  });
+});
+
 app.post("/api/insert", (req, res) => {
   const movieName = req.body.movieName;
   const movieReview = req.body.movieReview;
@@ -36,4 +43,22 @@ app.get("/test", (req, res) => {
 
 app.listen(3001, () => {
   console.log("running on port 3001");
+});
+
+app.delete("/api/delete/:movieName", (req, res) => {
+  const name = req.params.movieName;
+  const sqlDelete = "DELETE FROM movie_reviews WHERE movieName = ?";
+  db.query(sqlDelete, name, (error, result) => {
+    if (error) console.log(error);
+  });
+});
+
+app.put("/api/update", (req, res) => {
+  const review = req.body.movieReview;
+  const name = req.body.movieName;
+  const sqlUpdate =
+    "UPDATE movie_reviews SET movieReview = ? WHERE movieName = ?";
+  db.query(sqlUpdate, [review, name], (error, result) => {
+    if (error) console.log(error);
+  });
 });
